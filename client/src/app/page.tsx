c5e26e0d86c21c7d25cf103c7240096f353eb73a
Alex Zhang <alexzhang1618@gmail.com>
2024-04-19 23:14:05 -0700

    Add prettier, finish frontpage

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,32 +1,39 @@
-'use client'
+"use client";
 import Image from "next/image";
 import styles from "./page.module.css";
 import Typography from "@/components/Typography";
 import Link from "next/link";
-import Logo from '@/public/logo.svg'
+import Logo from "@/public/logo.svg";
 import { useState } from "react";
 
 export default function Home() {
-  const [code, setCode] = useState<string>('');
+  const [code, setCode] = useState<string>("");
 
   return (
     <main className={styles.main}>
       <div className={styles.container}>
-      <div className={styles.logo}>
-        <Image src="/logo.svg" width={500} height={300}/>
-      </div>
-      <div className={styles.options}>
-        <Link href='/create' className={styles.option}>
-          <Typography variant="body">create a room.</Typography>
-        </Link>
-        <div className={styles.join}>
-        <Link href={`/game/${code}`} className={styles.option}>
-          <Typography variant="body">join a room.</Typography>
-        </Link>
-          <label className={styles.joinLabel}>
-            code: 
-          <input type="text" value={code} onChange={(e) => setCode(e.target.value)} className={styles.joinInput}/></label></div>
-      </div>
+        <div className={styles.logo}>
+          <Image src="/logo.svg" width={500} height={300} alt="tempo logo." />
+        </div>
+        <div className={styles.options}>
+          <Link href="/create" className={styles.option}>
+            <Typography variant="body">create a room.</Typography>
+          </Link>
+          <div className={styles.option}>
+            <Link href={`/game/${code}`}>
+              <Typography variant="body">join a room.</Typography>
+            </Link>
+            <label className={styles.joinLabel}>
+              code:
+              <input
+                type="text"
+                value={code}
+                onChange={(e) => setCode(e.target.value)}
+                className={styles.joinInput}
+              />
+            </label>
+          </div>
+        </div>
       </div>
     </main>
   );
--- client/src/components/Typography/index.tsx
+++ client/src/components/Typography/index.tsx
@@ -1,28 +1,37 @@
-import { ReactNode } from 'react';
-import styles from './style.module.scss'
+import { ReactNode } from "react";
+import styles from "./style.module.scss";
 
 interface TypographyProps {
-    children: ReactNode;
-    variant: 'header' | 'body' | 'label';
-    bold?: boolean;
-    className?: string;
+  children: ReactNode;
+  variant: "header" | "body" | "label";
+  bold?: boolean;
+  className?: string;
 }
 
-const Typography = ({children, variant, bold, className} : TypographyProps) => {
-    
-    const variantToStyle = (): string => {
-        switch (variant) {
-            case 'header':
-                return styles.header
-            case 'body':
-                return styles.body
-            case 'label':
-                return styles.label
-        }
-    } 
-    return <div style={bold ? {fontWeight: 700} : undefined} className={`${variantToStyle()} ${className || ''}`}>
-        {children}
+const Typography = ({
+  children,
+  variant,
+  bold,
+  className,
+}: TypographyProps) => {
+  const variantToStyle = (): string => {
+    switch (variant) {
+      case "header":
+        return styles.header;
+      case "body":
+        return styles.body;
+      case "label":
+        return styles.label;
+    }
+  };
+  return (
+    <div
+      style={bold ? { fontWeight: 700 } : undefined}
+      className={`${variantToStyle()} ${className || ""}`}
+    >
+      {children}
     </div>
-}
+  );
+};
 
 export default Typography;
