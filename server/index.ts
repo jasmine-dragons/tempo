728f783220af2bbd4b61eb67a33c430278de61c4
Nishant Balaji <44332326+nishantbalaji@users.noreply.github.com>
2024-04-20 00:08:44 -0700

    server start (#2)

---
+++ server/index.ts
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const express = require("express");
+const app = express();
+import router from "./api";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const server = createServer(app);
+const port = process.env.PORT || 8000;
+
+const io = new Server(server, {
+  // options
+});
+
+io.on("connection", (socket) => {
+  // ...
+  console.log("connection started!");
+});
+
+dotenv.config();
+
+mongoose.connect(process.env.DB_URL).then(() => {
+  console.log("Connected to MongoDB database");
+});
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use("/", router);
+
+server.listen(port, () => {
+  console.log("Server started on port 8000!");
+});
