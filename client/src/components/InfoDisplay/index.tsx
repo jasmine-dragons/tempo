5e76882c2c5f694556bfddc116df6a98ed005e80
jenetic <jznnylam@gmail.com>
2024-04-20 01:59:48 -0700

    Add info component

---
+++ client/src/components/InfoDisplay/index.tsx
@@ -0,0 +1,12 @@
+import styles from "./style.module.scss";
+import Typography from "../Typography";
+
+const InfoDisplay = () => {
+  return (
+    <div className={styles.container}>
+      <div>hi</div>
+    </div>
+  );
+};
+
+export default InfoDisplay;
