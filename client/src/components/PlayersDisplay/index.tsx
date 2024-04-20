a575ee67d56391650cdcd9744555806cd1bb2e36
Alex Zhang <alexzhang1618@gmail.com>
2024-04-20 00:34:01 -0700

    style up players display

--- client/src/components/PlayersDisplay/index.tsx
+++ client/src/components/PlayersDisplay/index.tsx
@@ -1,6 +1,7 @@
 import { Player } from "@/types";
 import styles from './style.module.scss'
 import Typography from "../Typography";
+import Image from "next/image";
 
 interface PlayersDisplayProps {
     players: Player[];
@@ -8,10 +9,12 @@ interface PlayersDisplayProps {
 
 const PlayersDisplay = ({players}: PlayersDisplayProps) => {
     return <div className={styles.container}>
-        <Typography variant="subheader" bold>{`PLAYERS (${players.length})`}</Typography>
-        {players.map(player => 
-            <div key={player.uuid} className={styles.player}>
+        <Typography variant="subheader" bold>{`players.`}</Typography>
+        {players.map((player, i) => 
+            <div key={player.uuid} className={styles.playerCard}>
+                <Image src={`/players/${i}.svg`} width={75} height={75} alt={`${player.name}'s avatar'`} />
                 <Typography variant="subheader">{player.name}</Typography>
+                {player.isLeader ? <Image src="/icons/crown.svg" width={40} height={40} alt="crown" />: null}
             </div>
         )}
     </div>
