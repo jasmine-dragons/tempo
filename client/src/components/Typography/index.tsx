6a4a136823abe5a7cab3c4244f988bd630bc7315
Alex Zhang <alexzhang1618@gmail.com>
2024-04-19 23:06:26 -0700

    Add frontpage code

---
+++ client/src/components/Typography/index.tsx
@@ -0,0 +1,28 @@
+import { ReactNode } from 'react';
+import styles from './style.module.scss'
+
+interface TypographyProps {
+    children: ReactNode;
+    variant: 'header' | 'body' | 'label';
+    bold?: boolean;
+    className?: string;
+}
+
+const Typography = ({children, variant, bold, className} : TypographyProps) => {
+    
+    const variantToStyle = (): string => {
+        switch (variant) {
+            case 'header':
+                return styles.header
+            case 'body':
+                return styles.body
+            case 'label':
+                return styles.label
+        }
+    } 
+    return <div style={bold ? {fontWeight: 700} : undefined} className={`${variantToStyle()} ${className || ''}`}>
+        {children}
+    </div>
+}
+
+export default Typography;
