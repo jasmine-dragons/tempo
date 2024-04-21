b47b01dab08cb327419ec3041d17b17b75d48cb8
Nishant Balaji <44332326+nishantbalaji@users.noreply.github.com>
2024-04-20 19:14:40 -0700

    unused socket cleanup (#17)

--- server/index.ts
+++ server/index.ts
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const express = require("express");
 const app = express();
-const cors = require('cors')
+const cors = require("cors");
 import router from "./api";
 import { createServer } from "http";
 import { Server } from "socket.io";
@@ -13,44 +13,51 @@ const server = createServer(app);
 const port = process.env.PORT || 8000;
 
 const gameState = {
-  state: 'waiting',
+  state: "waiting",
   players: [],
 };
 
 const io = new Server(server, {
   // options
   cors: {
-    origin: "*"
-  }
+    origin: "*",
+  },
 });
 
 io.on("connect", (socket) => {
   // ...
   gameState.players.push(socket);
 
-  socket.on('player join', ( { name, room }) => {
+  socket.on("player join", ({ name, room }) => {
     const { user, error } = addUser(socket.id, name, room);
     if (error) return;
     //gameState.players.push({ id: socket.id, name, room });
     socket.join(user.room);
-    socket.in(room).emit('notification', { title: 'Someone\'s here', description: `${user.name} just entered the room`});
-    io.in(room).emit('users', getUsers(room));
+    socket
+      .in(room)
+      .emit("notification", {
+        title: "Someone's here",
+        description: `${user.name} just entered the room`,
+      });
+    io.in(room).emit("users", getUsers(room));
     //io.emit('gameState', gameState);
   });
 
-  socket.on('game start', () => {
-    gameState.state = 'playing';
-    io.emit('gameState', gameState);
+  socket.on("game start", () => {
+    gameState.state = "playing";
+    // io.emit('gameState', gameState);
   });
 
-  socket.on('judging', () => {
-    gameState.state = 'judging';
-    io.emit('gameState', gameState);
+  socket.on("judging", () => {
+    gameState.state = "judging";
+    // io.emit('gameState', gameState);
   });
 
   socket.on("disconnect", () => {
-    gameState.players = gameState.players.filter((player) => player.id !== socket.id);
-    io.emit("gameState", gameState);
+    gameState.players = gameState.players.filter(
+      (player) => player.id !== socket.id
+    );
+    // io.emit("gameState", gameState);
   });
 
   console.log(`connection: ${socket.id}!`);
@@ -64,11 +71,11 @@ mongoose.connect(process.env.DB_URL).then(() => {
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
 app.use("/", router);
 
 server.listen(port, () => {
   console.log("Server started on port 8000!");
 });
-io.listen(9000)
+io.listen(9000);
