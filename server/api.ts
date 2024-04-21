52ef62f0598cec8e1acd78eac0681e6847f4e82c
Nikhil Dange <nsdange@ucsd.edu>
2024-04-21 00:00:03 -0700

    route and logic for blob uploads to mongodb

--- server/api.ts
+++ server/api.ts
@@ -3,6 +3,11 @@ import * as uuid from 'uuid';
 const {GameSession} = require('./models/gameSession');
 
 
+
+
+// const upload = multer({ storage: multer.memoryStorage() });
+
+
 const router = express.Router();
 
 router.get("/game/:id", async (req, res) => {
@@ -55,7 +60,7 @@ router.post('/game', async (req, res) => {
 });
 
 // submit the game for a particular round
-router.post('/game/:id/round/:round', async (req, res) => {
+router.post('/game/:id/submit', async (req, res) => {
 
   // find a game based on the session id
   const game = await GameSession.findOne({ sessionId: req.params.id });
@@ -64,31 +69,25 @@ router.post('/game/:id/round/:round', async (req, res) => {
     return res.status(404).json({ error: "Game not found" });
   }
 
-  // find the user in the game session
-  const user = game.users.find(u => u.user === req.body.user);
+  // old upload 
+  // const blob = req.file.buffer;
+  // const buffer = Buffer.from(blob, 'binary');
+  // game.blob = buffer;
+  // await game.save();
 
-  if (!user) {
-    return res.status(404).json({ error: "User not found" });
-  }
 
-  // check if the user has already submitted for this round
-  const round = user.audioFiles.find(a => a.round === req.params.round);
-
-  if (round) {
-    return res.status(400).json({ error: "User has already submitted for this round" });
-  }
-  
-  // add the audio file to the user
-  user.audioFiles.push({
-    round: req.params.round,
-    audioData: req.body.audioData,
-    mimeType: req.body.mimeType
-  });
-
+  const buffer = req.body;
+  game.musicBlob = buffer;
   await game.save();
 
-  res.status(200).json({ game });
+  // const wavFile = req.file;
+  // console.log(req.file);
+  // //Buffer.from(new Uint8Array(wavData))
+  // game.musicBlob = wavFile.buffer;
+  // await game.save();
 
+
+  res.status(200).json({ game });
 });
 
 
